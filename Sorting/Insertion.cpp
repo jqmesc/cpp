8720681be3743f6c93026ce5719c997579e81512
james <jqmessc@gmail.com>
2023-12-21 18:36:35 -0800

    Advent day 1

--- Sorting/Insertion.cpp
+++ Sorting/Insertion.cpp
@@ -6,34 +6,34 @@
 using namespace std;
 
 int findPos(vector<int> &v,int p){
-	for (int i=0;i<v.size();i++){
-		if (v[i]>p||v[i]==p){
-			return i;
-		}
-	}
-	return v.size();
+    for (int i=0;i<v.size();i++){
+        if (v[i]>p||v[i]==p){
+            return i;
+        }
+    }
+    return v.size();
 }
 
 void Sort(vector<int> &v){
-	for (int i=0;i<v.size();i++){
-		int p=findPos(v,v[i]);
-		v.insert(v.begin()+p,v[i]);
-		v.erase(v.begin()+i+1);
-	}
+    for (int i=0;i<v.size();i++){
+        int p=findPos(v,v[i]);
+        v.insert(v.begin()+p,v[i]);
+        v.erase(v.begin()+i+1);
+    }
 }
 
 int main() {
     vector<int> v={9,32,8,49,20,1}; // Unsorted
 
-	cout<<"Unsorted:"<<endl;
+    cout<<"Unsorted:"<<endl;
     for (int i=0;i<v.size();i++){
-    	cout<<v[i]<<" ";
+        cout<<v[i]<<" ";
     }
     cout<<endl;
     Sort(v);
-	cout<<"Sorted:"<<endl;
+    cout<<"Sorted:"<<endl;
     for (int i=0;i<v.size();i++){
-    	cout<<v[i]<<" ";
+        cout<<v[i]<<" ";
     }
     cout<<endl;
     return 0;
