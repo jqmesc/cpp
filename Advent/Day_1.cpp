c665e52c20229e173e9e5295e351654f0cd523fe
james <jqmessc@gmail.com>
2023-12-22 02:50:49 -0800

    Day 1 part 2

--- Advent/Day_1.cpp
+++ Advent/Day_1.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <regex>
+#include <map>
 using namespace std;
 
 const vector<string> inputs={
@@ -1007,12 +1008,61 @@ const vector<string> inputs={
 	"9one9pjtnncsqzhcszp5",
 };
 
+/*
 int main(){
 	int total=0;
 	regex match("\\d");
 	for (auto v:inputs){
 		smatch base;
 		vector<string> result;
+		while (regex_search(v,base,match)){
+			result.push_back(base[0]);
+			v=base.suffix().str();
+		}
+		string First=result[0];
+		string Last=result[result.size()-1];
+		string Combined=First+Last;
+		int Num=stoi(Combined);
+		total+=Num;
+	}
+	cout<<total;
+	return 0;
+}
+*/
+
+// Day 2
+int main(){
+	map<string,string> s={
+		{"oneight","18"},
+		{"twone","21"},
+		{"threeight","38"},
+		{"fiveight","58"},
+		{"nineight","98"},
+		{"eightwo","82"},
+		{"eighthree","83"},
+	};
+	map<string,string> m={
+		{"one","1"},
+		{"two","2"},
+		{"three","3"},
+		{"four","4"},
+		{"five","5"},
+		{"six","6"},
+		{"seven","7"},
+		{"eight","8"},
+		{"nine","9"},
+	};
+	int total=0;
+	regex match("\\d");
+	for (auto v:inputs){
+		smatch base;
+		vector<string> result;
+		for (auto r:s){
+			v=regex_replace(v,regex(r.first),r.second);
+		}
+		for (auto r:m){
+			v=regex_replace(v,regex(r.first),r.second);
+		}
 		while (regex_search(v,base,match)){
 			result.push_back(base[0]);
 			v=base.suffix().str();
